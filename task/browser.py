4ed25c8e6a5f188cef53d381cc2d8a269ae8d448
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-09-05 14:09:28 +0200

    Implement fourth step "Address line"

--- task/browser.py
+++ task/browser.py
@@ -1,41 +1,8 @@
 import os
 import sys
 from collections import deque
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created "soft" magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import requests
+from bs4 import BeautifulSoup
 
 
 def try_exit(text):
@@ -55,19 +22,6 @@ def try_back(text, history):
     return text
 
 
-def is_correct(text):
-    return '.' in text
-
-
-def contains(text, cur_list):
-    return text in cur_list
-
-
-def file_name(text):
-    dot_index = text.index('.')
-    return text[:dot_index]
-
-
 def work_with_dir():
     dir_path = sys.argv[1]
     if not os.path.exists(dir_path):
@@ -75,19 +29,35 @@ def work_with_dir():
     os.chdir(dir_path)
 
 
-def work_with_files(name, stack):
-    websites = {'bloomberg.com': bloomberg_com, 'nytimes.com': nytimes_com}
+def add_protocol(text):
+    if not text.startswith('http://'):
+        text = 'http://' + text
 
+    return text
+
+
+def is_correct(text):
+    return '.' in text
+
+
+def file_name(text):
+    dot_index = text.index('.')
+    return text[:dot_index]
+
+
+def work_with_files(name, history):
     if os.path.exists(name):
         with open(name, 'r', encoding='utf-8') as old_f:
             for line in old_f:
                 print(line, end='')
             print()
-    elif is_correct(name) and contains(name, websites.keys()):
+    elif is_correct(name):
         with open(file_name(name), 'w', encoding='utf-8') as new_f:
-            print(websites[name])
-            new_f.write(websites[name])
-            stack.append(name)
+            history.append(file_name(name))
+            r = requests.get(add_protocol(name))
+            new_f.write(r.text)
+            print(r.text)
+
     else:
         print('Invalid URL')
 
